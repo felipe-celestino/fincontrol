5de827526d12662a8ba7d5e2b61454ef8d2877ee
felipe <felipe.ceslestino@gmail.com>
2023-12-12 21:34:55 -0300

    segue pagina do historico

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("mes").addEventListener("change", mudarTexto);
+
+    function mudarTexto() {
+      var select = document.getElementById("mes");
+      var selectedValue = select.options[select.selectedIndex].text;
+
+      document.getElementById("textoMudado").textContent = "Historico de " + selectedValue;
+    }
+  });
 console.log("Bem vindos!")
